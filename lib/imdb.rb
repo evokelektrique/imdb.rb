69c1d184a98da1350ada1ddadf3f6ae72d6b64ad
EVOKE <evoke.lektrique@gmail.com>
2021-10-31 00:29:27 +0330

    First commit

---
+++ lib/imdb.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "imdb/version"
+
+module Imdb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/imdb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Imdb do
+  it "has a version number" do
+    expect(Imdb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
